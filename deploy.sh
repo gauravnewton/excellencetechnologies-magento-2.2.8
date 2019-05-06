e19eb6b7ba6c176d5e8e9cae5b431a4c86f39aff
Gaurav <gaurav@excellencetechnologies.in>
2019-05-06 11:11:36 +0530

    added a new_branch

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+sudo php bin/magento setup:upgrade
+
+sudo php bin/magento setup:static-content:deploy -f
+
+sudo chmod -R 777 var/ pub/ generated/
+
+sudo php bin/magento c:c
+
+sudo php bin/magento c:f
+
+<<<<<<< HEAD
+sudo chmod -R 777 var/ pub/ generated/
+=======
+sudo chmod -R 777 var/ pub/ generated/
+>>>>>>> daf35a11b6fdf6d79592c09d54e13ad0756d0155
