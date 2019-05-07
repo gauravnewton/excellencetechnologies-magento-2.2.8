4993796bfdb815646aea21fa0f5a203006c0daaf
Gaurav <gaurav@excellencetechnologies.in>
2019-05-07 18:19:59 +0530

    worked on header

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,6 @@ sudo php bin/magento c:c
 
 sudo php bin/magento c:f
 
-<<<<<<< HEAD
 sudo chmod -R 777 var/ pub/ generated/
-=======
+
 sudo chmod -R 777 var/ pub/ generated/
->>>>>>> daf35a11b6fdf6d79592c09d54e13ad0756d0155
